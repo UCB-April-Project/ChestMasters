de038c9b8e90b72bfc6e4f84ba67558c390261d6
Scott Ford <uofoduck14@gmail.com>
2019-08-12 18:37:44 +0000

    forgot to save file. this push is to ensure all code is correctly pushed.

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -2,35 +2,47 @@ class ChessPiece < ApplicationRecord
     belongs_to :game
 
     def is_obstructed?(x, y)
-        game.chess_piece.where("x_pos = ? and y_pos = ?", x, y).present?
+       game.chess_piece.where("x_pos = ? and y_pos = ?", x, y).present?
     end
 
     def vert_obstructed?(x, y)
         sx = x_pos
         sy = y_pos
 
-        if y =! sy && x === sx
+        while y =! sy && x === sx
             y_move = (y <=> sy)
             sy += y_move
+            return true if is_obstructed?(x, y_move)
         end
-        return true if is_obstructed?(x, y_move)
+        false
     end
 
     def horz_obstructed?(x, y) 
         sx = x_pos
         sy = y_pos       
 
-        if x =! sx && y === sy
+        while x =! sx && y === sy
             x_move = (x <=> sx)
             sx += x_move
+            return true if is_obstructed?(x_move, y)
         end
-        return true if is_obstructed?(x_move, y)
+        false
     end
 
     def diag_obstructed?(x, y)
+        x_spot = x_pos < x ? 1 : -1
+        y_spot = y_pos < y ? 1 : -1
 
+        sx = x_pos + x_spot
+        sy = y_pos + y_spot
+
+        while sx =! x && sy =! y
+            sx += x_spot
+            sy += y_spot
+            return true if is_obstructed?(sx, sy)
+        end
+        false
     end
-
     
 
 end
