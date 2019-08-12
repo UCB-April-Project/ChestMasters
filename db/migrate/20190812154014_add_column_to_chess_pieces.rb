cd8b11d54a51a7218a762d81f4a51223d7ab762c
Scott Ford <uofoduck14@gmail.com>
2019-08-12 16:12:03 +0000

    corrected the added columns from strings to integers

--- db/migrate/20190812154014_add_column_to_chess_pieces.rb
+++ db/migrate/20190812154014_add_column_to_chess_pieces.rb
@@ -1,6 +1,6 @@
 class AddColumnToChessPieces < ActiveRecord::Migration[5.2]
   def change
-    add_column: :chess_piece, :x_pos, :string
-    add_column: :chess_piece, :y_pos, :string 
+    add_column: :chess_piece, :x_pos, :integer
+    add_column: :chess_piece, :y_pos, :integer 
   end
 end
