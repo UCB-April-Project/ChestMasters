daaf11f9b77324a15e6c026116e5695c8a3b5c96
Scott Ford <uofoduck14@gmail.com>
2019-08-12 18:32:19 +0000

    added 4 methods for horizontal, vertical, and diagonal obstructions

--- app/models/chess_piece.rb
+++ app/models/chess_piece.rb
@@ -1,22 +1,37 @@
 class ChessPiece < ApplicationRecord
     belongs_to :game
 
-    def vert_obstructed?(x, y)
-
+    def is_obstructed?(x, y)
+        game.chess_piece.where("x_pos = ? and y_pos = ?", x, y).present?
     end
 
-    def horz_obstructed?(x, y)
-        
+    def vert_obstructed?(x, y)
+        sx = x_pos
+        sy = y_pos
 
+        if y =! sy && x === sx
+            y_move = (y <=> sy)
+            sy += y_move
+        end
+        return true if is_obstructed?(x, y_move)
+    end
+
+    def horz_obstructed?(x, y) 
+        sx = x_pos
+        sy = y_pos       
+
+        if x =! sx && y === sy
+            x_move = (x <=> sx)
+            sx += x_move
+        end
+        return true if is_obstructed?(x_move, y)
     end
 
     def diag_obstructed?(x, y)
 
     end
 
-    def is_obstructed?(x, y)
-        chess_pieces.where(x_pos: x, y_pos: y).last
-    end
+    
 
 end
 
