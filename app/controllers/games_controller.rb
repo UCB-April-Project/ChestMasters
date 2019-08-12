acda74dcacc9f70c7a937c429122164e3bd0a810
Scott Ford <uofoduck14@gmail.com>
2019-08-12 19:18:04 +0000

    Added avilable games to index page.

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,6 +1,14 @@
 class GamesController < ApplicationController
 
   def index
+    @games = Game.all
+  end
+
+  def new
+    @game = Game.new
+  end
+
+  def create 
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_10_025134) do
+ActiveRecord::Schema.define(version: 2019_08_12_154014) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_08_10_025134) do
     t.string "type"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "x_pos"
+    t.integer "y_pos"
   end
 
   create_table "games", force: :cascade do |t|
