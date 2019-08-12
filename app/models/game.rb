c4e2a8bc6964912a57c81fb9892008657770b75b
Scott Ford <uofoduck14@gmail.com>
2019-08-12 18:58:44 +0000

    added avialble scope for games

--- app/models/game.rb
+++ app/models/game.rb
@@ -6,5 +6,7 @@ class Game < ApplicationRecord
     #belongs_to :user
     #uncomment when the user table is created for device
     has_many :chess_pieces
+    scope :available, -> { where("white => IS NULL AND black => IS NULL") OR ("white => IS NOT NULL AND black => IS NULL")}
+    
 
 end
