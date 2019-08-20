3c9f67258d7b799fbde4746fdef97ba166d5a3ba
Austin Conner <abconner25@gmail.com>
2019-08-20 06:22:30 +0000

    deleted redundant tables in merge

--- db/migrate/20190813222815_update_chess_piece_postion.rb
+++ db/migrate/20190813222815_update_chess_piece_postion.rb
@@ -1,8 +1,5 @@
 class UpdateChessPiecePostion < ActiveRecord::Migration[5.2]
   def change
-    add_column :chess_pieces, :x_pos, :integer
-    add_column :chess_pieces, :y_pos, :integer
     remove_column :chess_pieces, :position
-
   end
 end
