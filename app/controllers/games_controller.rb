de56810955b71461369729a36204c898a736b808
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-08-21 01:43:12 +0000

    add method into game controller

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,27 @@
 class GamesController < ApplicationController
 
+
   def index
-     @matches = Matches.all
+     @games = Game.all
   end
 
   def new
-     @matches = Matches.new
+     @games = Game.new
   end
- 
-     
+
+  def create
+    @games = Game.new(games_params)
+
+    respond_to do |format|
+      if @games.save
+        format.html { redirect_to @game, notice: 'game was successfully created.' }
+        format.json { render :show, status: :created, location: @game }
+      else
+        format.html { render :new }
+        format.json { render json: @game.errors, status: :unprocessable_entity }
+      end
+    end
+  end   
  end
 
 
