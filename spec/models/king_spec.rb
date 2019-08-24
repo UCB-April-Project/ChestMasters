088ec2be0a1ebdfc51258d0c61cfb6a0d3ae37a4
kathleen kalugdan <kay16_alcantara@yahoo.com.ph>
2019-08-24 18:01:48 +0000

    added test to check if it returns false for invaid moves

--- spec/models/king_spec.rb
+++ spec/models/king_spec.rb
@@ -2,31 +2,42 @@ require 'rails_helper'
 
 RSpec.describe King, type: :model do
   describe "#valid move?" do
-    it "should return true to move one square up" do
+    it "should return true to move one square left" do
       game = Game.create
       king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
       expect(king.valid_move?(5, 4)).to eq(true)
     end
 
-     it "should return true to move one square down" do
+     it "should return true to move one square right" do
       game = Game.create
       king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
       expect(king.valid_move?(5, 6)).to eq(true)
     end
 
-     it "should return true to move one square to the left" do
+     it "should return true to move one square to the up" do
       game = Game.create
       king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
       expect(king.valid_move?(4, 5)).to eq(true)
     end
 
-     it "should return true to move one square to the right" do
+     it "should return true to move one square to the down" do
       game = Game.create
       king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
       expect(king.valid_move?(6, 5)).to eq(true)
     end
 
+      it "should return true to move one square diagonally down to the right" do
+      game = Game.create
+      king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
+      expect(king.valid_move?(6, 6)).to eq(true)
+      end
+
+      it "should return false to move two squares down" do
+      game = Game.create
+      king = FactoryBot.create :king, x_pos: 5, y_pos: 5, game_id: game.id
+      expect(king.valid_move?(7, 5)).to eq(false)
+    end
+
 
   end
-
 end
