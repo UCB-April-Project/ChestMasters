01b9df45fca4ace8a0dbc9d389b286f40ca32c82
gladieschanggoodluck <gladiesgoodluck@gmail.com>
2019-08-24 00:37:45 +0000

    add board_state update method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,14 +1,45 @@
 class GamesController < ApplicationController
 
   def index
-    @games = Game.available
+      
   end
 
   def new
-    @game = Game.new
+     @games = Game.new
   end
 
-  def create 
+
+  def show
   end
+
+  def update
+      if game.valid? 
+        game.update_attributes matches_params
+        game.assign_chess_pieces
+        return redirect_to game_path game
+      end
+
+      render :new, status: unprocessable_entity 
+  end  
+   
+  
+  def game
+      @matches ||= Matches.where(id: params[:id]).last
+  end
+  
+  def matches_params
+      params.require( :matches).permit(
+        :board_state,
+        :white,
+        :black)    
+  end
+
+
+  def create
+    @game =Game.create(game_params)
+    redirect_to game_path(@game)
+  end
+
+
 
 end
