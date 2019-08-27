a258595e41c70a7ebfa6db71fe16fe242cc60b8c
Austin Conner <abconner25@gmail.com>
2019-08-27 17:59:26 +0000

    setup valid move for bishop

--- app/models/bishop.rb
+++ app/models/bishop.rb
@@ -5,4 +5,13 @@ class Bishop < ChessPiece
     #move(coordinates): moves piece to a given location on board
     #destroy(): for when a piece is captured
 
+    def valid_move?(x_move, y_move)
+
+        #find slope of line between goal and current location
+        slope = (y_move - y_pos)/(x_move - x_pos)
+
+        #valid move if slope is 1 or -1
+        return (slope.abs == 1)
+    end
+
 end
