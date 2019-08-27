a9087e09834c97e31d81f0c1047e620c279c7592
Austin Conner <abconner25@gmail.com>
2019-08-27 18:01:18 +0000

    setup valid move for queen

--- app/models/queen.rb
+++ app/models/queen.rb
@@ -5,4 +5,14 @@ class Queen < ChessPiece
     #move(coordinates): moves piece to a given location on board
     #destroy(): for when a piece is captured
 
+    def valid_move?(x_move, y_move)
+
+        #find slope of line between goal and current location
+        slope = (y_move - y_pos)/(x_move - x_pos)
+
+        #valid move if slope is 1 or -1
+        #also valid if x or y is 0
+        return (slope.abs == 1 || x_move == 0 || y_move == 0)
+    end
+
 end
