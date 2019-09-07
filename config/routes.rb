21d74a9de3f186fc5cbd03f18a167a8b1bfef715
abconner25 <33856885+abconner25@users.noreply.github.com>
2019-09-06 19:27:19 -0700

    Pieces sud (#26)
    * setup Show, Update, and Destroy actions for chess pieces
    
    * fixed issue with update action
    
    * piece show action works but the test currently fails

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'users#index'
-  resources :games, only: [:show, :index]
+  resources :games, only: [:show, :index] do
+    resources :chess_pieces, only: [:show, :update, :destroy]
+  end
 end
